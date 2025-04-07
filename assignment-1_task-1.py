737765b6d0deacceea42284e0aa0729e2b07dde8
Qasim <120741312+qasim8786@users.noreply.github.com>
2025-04-07 10:18:17 +0530

    Add files via upload

---
+++ assignment-1_task-1.py
@@ -0,0 +1,8 @@
+#Taking inputs
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+#Calculations + Printing results
+print("Addition: ", a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Devision: ", a/b)
